4a2ac7f6330ad21f9ad41c3ec3fb5319a7de4abe
Cebans <julestrodriguez@uniboyaca.edu.co>
2022-05-26 23:10:43 -0500

    cambios js primeros

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const displayvaloractual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('numero');
 const botonesOperadores = document.querySelectorAll('operador');
 
+const display = new Display(displayvaloranterior,displayvaloractual);
 
+botonesNumeros.forEach(boton => {
+    boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
+});
 
