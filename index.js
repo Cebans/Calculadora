37f7460c8b2ffe366dadb230f51d46322c1821c2
Cebans <julestrodriguez@uniboyaca.edu.co>
2022-05-26 22:32:25 -0500

    js

---
+++ index.js
@@ -0,0 +1,7 @@
+const displayvaloranterior = document.getElementById('valor-anterior');
+const displayvaloractual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('numero');
+const botonesOperadores = document.querySelectorAll('operador');
+
+
+
