28cdd578ccbc209def5e56960be4900a1c45f1ee
Your Name <lfotalora@uniboyaca.edu>
2022-05-27 20:45:33 -0500

    cambios no sirven botones

--- Display.js
+++ Display.js
@@ -3,16 +3,43 @@ class Display {
         this.displayValorActual = displayValorActual;
         this.displayValorAnterior = displayValorAnterior;
         this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
         this.valorActual = '';
         this.valorAnterior = '';
     }
 
+    borrar(){
+        this.valorActual = this.valorActual.slice(0,-1);
+        this.imprimirValores();
+    }
+
+    borrarTodo(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
     agregarNumero(numero){
-        this.valorActual = numero;
+        this.valorActual += numero;
+        this.imprimirValores();
     }
 
     imprimirValores(){
         this.displayValorActual.textContent = this.valorActual;
         this.displayValorAnterior.textContent = this.valorAnterior;
+    }
+
+    calcular(){
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior,this.valorActual);
+    }
+
+    computar(tipoOperacion){
+        if(this.tipoOperacion !== 'igual' && this.calcular()){
+            this.tipoOperacion = tipoOperacion;
+            this.valorAnterior = this.valorActual || this.valorAnterior;
+            this.valorActual = '';
+            this.imprimirValores();
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const displayvaloranterior = document.getElementById('valor-anterior');
-const displayvaloractual = document.getElementById('valor-actual');
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
 const botonesNumeros = document.querySelectorAll('numero');
 const botonesOperadores = document.querySelectorAll('operador');
 
-const display = new Display(displayvaloranterior,displayvaloractual);
+const display = new Display(displayValorAnterior,displayValorActual);
 
 botonesNumeros.forEach(boton => {
     boton.addEventListener('click', () => display.agregarNumero(boton.innerHTML));
 });
 
+botonesOperadores.forEach(boton => {
+    boton.addEventListener('click', () => display.computar(boton.value));
+});
